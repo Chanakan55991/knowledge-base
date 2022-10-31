15d59190c44101d5cfcfb7e3f0499e78f6563b5e
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-10-31 18:27:53 +0700

    refactor(*): refactored to knowledge base

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog - Chanakan Mungtin';
-export const SITE_DESCRIPTION = 'A random blog posts I made from time to time';
+export const SITE_TITLE = 'KB - Chanakan Mungtin';
+export const SITE_DESCRIPTION = 'A knowledge base for all my classes content';
 export const HOMEPAGE_URL = 'https://www.chanakancloud.net'
---
+++ src/pages/posts/swot.md
@@ -0,0 +1,17 @@
+---
+description: Business Study - SWOT
+slug: swot
+public: true
+layout: ../../layouts/BlogPost.astro
+title: SWOT - Business Study
+createdAt: 1648507383713
+updatedAt: 1667215870782
+tags:
+  - General
+heroImage: /astrojs.jpg
+---
+## SWOT คืออะไร
+- **S - Strength**
+- **W - Weaknesses**
+- **O - Opportunities**
+- **T - Threats**
